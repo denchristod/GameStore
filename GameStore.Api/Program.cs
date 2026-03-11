8fa69e9017548fef7b60049c1d27d09c9ce3eb16
denchristod <den.christod@gmail.com>
2026-03-11 13:37:31 +0100

    Added error handling for non-existent resources

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography.X509Certificates;
 using GameStore.Api.Dtos;
 
 const string GetGameEndpointName = "GetGame";
@@ -31,8 +32,13 @@ List<GameDto> games = [
 app.MapGet("/games", () => games);
 
 // GET /games/id
-app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id))
-    .WithName(GetGameEndpointName);
+app.MapGet("/games/{id}", (int id) => 
+{
+    var game = games.Find(game => game.Id == id);
+
+    return game is null ? Results.NotFound() : Results.Ok(game);
+})
+.WithName(GetGameEndpointName);
 
 // POST /games
 app.MapPost("/games", (CreateGameDto newGame) =>
@@ -54,6 +60,11 @@ app.MapPost("/games", (CreateGameDto newGame) =>
 app.MapPut("/games/{id}", (int id, UpdateGameDto updatedGame) =>
 {
     var index = games.FindIndex(game => game.Id == id);
+
+    if (index == -1)
+    {
+        return Results.NotFound();
+    }
 
     games[index] = new GameDto(
         id,
